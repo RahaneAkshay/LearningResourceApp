0814a0168d5ab1f4cacff6bb19a47d57e0487a3d
Akshay Rahane <akshay.rahane@perennialsys.com>
2021-11-16 12:48:04 +0530

    Root Component Added

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <ul v-for="(item,index) in storedResources" :key="index">
+        <li >{{item.id}}</li>
+         <li >{{item.title}}</li>
+          <li >{{item.description}}</li>
+           <li >{{item.link}}</li>
+    </ul>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            storedResources:[{
+                id:'vue',
+                title:'vue official guide',
+                description:'Use vue docs',
+                link:'https://v3.vuejs.org/'
+            },
+            {
+                id:'Google',
+                title:'Google',
+                description:'Google for help',
+                link:'https://www.google.co.in/'
+            }]
+        }
+    }
+}
+</script>
